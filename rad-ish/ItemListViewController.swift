cbb730e7ca5b181820700d418fe7d385844c1a68
tammy0508 <tamara.dandreamatteo@gmail.com>
2019-06-28 10:14:47 -0700

    Complete

---
+++ rad-ish/ItemListViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ItemListViewController.swift
+//  rad-ish
+//
+//  Created by Apple on 6/26/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    var foods = [FoodData]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foods.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //refers to each row- allows to use attributes
+        let cell = UITableViewCell()
+        let foodCell = foods[indexPath.row]
+        // the information displayed on the cell
+        cell.textLabel?.text = "\(foodCell.name!):  \(foodCell.time!) days till expiration"
+        
+        return cell
+    }
+
+    //allows reording of cells. Tells the computer we want the people to be able to reorder
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    //lets the user rearrange the cells
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = foods[sourceIndexPath.row]
+        foods.remove(at: sourceIndexPath.row)
+        foods.insert(item, at: destinationIndexPath.row)
+        
+    }
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
+    @IBAction func edit(_ sender: Any) {
+        // each time the user clicks the button the user is switching between editing and not editing
+        myTableView.isEditing = !myTableView.isEditing
+        
+        //changes the edit button to done when editing
+        switch myTableView.isEditing {
+        case true:
+            editButton.title = "done"
+        case false:
+            editButton.title = "edit"
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let rowToDelete = foods[indexPath.row]
+                context.delete(rowToDelete)
+            }
+        }
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        displayfoodItems()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        displayfoodItems()
+    }
+
+   
+
+    // allows to display data in core data
+    func displayfoodItems() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            if let coreDataFoodData = try? context.fetch(FoodData.fetchRequest()) as? [FoodData] {
+                foods = coreDataFoodData
+                myTableView.reloadData()
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
+
+
+    
+
---
+++ rad-ish/customizeButtonsViewController.swift
@@ -0,0 +1,87 @@
+//
+//  customizeButtonsViewController.swift
+//  rad-ish
+//
+//  Created by Apple on 6/27/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class customizeButtonsViewController: UIViewController {
+
+    var foods = [FoodData]()
+    
+    
+        // Do any additional setup after loading the view.
+    
+    
+
+    @IBOutlet weak var foodName: UITextField!
+    
+    @IBOutlet weak var expirationTime: UITextField!
+    
+    
+    
+    @IBAction func notification(_ sender: UIButton) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Your food is about to become expired"
+        content.subtitle = "Use your food now!"
+        content.body = "Open the app to see your items!"
+        content.badge = 1
+        
+        
+        let trigger = UNTimeIntervalNotificationTrigger( timeInterval: 60.0, repeats: true )
+        let request = UNNotificationRequest( identifier: "timerDoner", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert ,.sound,.badge ], completionHandler: {didAllow, error in})
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    // if button is pressed it will save the information in the core data
+    @IBAction func saveNewFood(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            let foodToSave = FoodData(entity: FoodData.entity(), insertInto: context)
+            
+            foodToSave.name = foodName.text
+            
+            foodToSave.time = expirationTime.text
+            
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            
+            navigationController?.popViewController(animated: true)
+        }
+        
+        
+    }
+
+
+}
+    
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
